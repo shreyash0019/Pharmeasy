64b8bb5babcad2c5b928e90b1c02ac22284f9f83
Shreyash Ingle <141054759+shreyash0019@users.noreply.github.com>
2026-03-22 22:59:28 +0530

    Update serializers.py

--- reminders/serializers.py
+++ reminders/serializers.py
@@ -1,8 +1,25 @@
 from rest_framework import serializers
 from .models import Reminder
 
+
 class ReminderSerializer(serializers.ModelSerializer):
+    # 🔹 readable user name
+    user_name = serializers.CharField(source='user.username', read_only=True)
+
     class Meta:
         model = Reminder
-        fields = '__all__'
-        read_only_fields = ['patient']
+        fields = [
+            'id',
+            'user',
+            'user_name',
+            'message',
+            'remind_at'
+        ]
+
+        read_only_fields = ['user']
+
+    # 🔥 auto assign logged-in user
+    def create(self, validated_data):
+        request = self.context.get('request')
+        validated_data['user'] = request.user
+        return super().create(validated_data)
