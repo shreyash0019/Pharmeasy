207d6b0e9a45529615c454f27b649b5aa3b61f72
Shreyash Ingle <141054759+shreyash0019@users.noreply.github.com>
2026-03-23 02:01:18 +0530

    Update views.py

--- pharmacy/views.py
+++ pharmacy/views.py
@@ -4,7 +4,6 @@ from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 from django.db.models import Q
 
-from .models import Medicine, StoreInventory, MedicalStore, Order, Reminder
 from .serializers import (
     MedicineSerializer,
     StoreInventorySerializer,
