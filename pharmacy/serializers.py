89a22ea46c3ec3df0278f73307d7614c357af323
Shreyash Ingle <141054759+shreyash0019@users.noreply.github.com>
2026-03-22 22:51:06 +0530

    Refactor serializers to specify fields explicitly

--- pharmacy/serializers.py
+++ pharmacy/serializers.py
@@ -2,20 +2,49 @@ from rest_framework import serializers
 from .models import Medicine, StoreInventory, MedicalStore
 
 
+# 💊 Medicine Serializer
 class MedicineSerializer(serializers.ModelSerializer):
     class Meta:
         model = Medicine
-        fields = '__all__'
+        fields = [
+            'id',
+            'name',
+            'description',
+            'requires_prescription',
+            'manufacturer',
+            'composition'
+        ]
 
 
+# 🏪 Medical Store Serializer
 class MedicalStoreSerializer(serializers.ModelSerializer):
+    user = serializers.StringRelatedField(read_only=True)
+
     class Meta:
         model = MedicalStore
-        fields = '__all__'
-        read_only_fields = ['user']
+        fields = [
+            'id',
+            'store_name',
+            'address',
+            'user'
+        ]
 
 
+# 📦 Store Inventory Serializer (IMPORTANT UPGRADE)
 class StoreInventorySerializer(serializers.ModelSerializer):
+    store_name = serializers.CharField(source='store.store_name', read_only=True)
+    medicine_name = serializers.CharField(source='medicine.name', read_only=True)
+
     class Meta:
         model = StoreInventory
-        fields = '__all__'
+        fields = [
+            'id',
+            'store',
+            'store_name',
+            'medicine',
+            'medicine_name',
+            'price',
+            'discount_price',
+            'stock',
+            'expiry_date'
+        ]
