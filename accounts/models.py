204232be4fc67c570125582308a33571727064d7
Shreyash Ingle <141054759+shreyash0019@users.noreply.github.com>
2026-03-22 23:10:28 +0530

    Update models.py

--- accounts/models.py
+++ accounts/models.py
@@ -1,9 +1,17 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+
 class User(AbstractUser):
     ROLE_CHOICES = (
         ('patient', 'Patient'),
         ('seller', 'Seller'),
     )
+
     role = models.CharField(max_length=20, choices=ROLE_CHOICES)
+
+    # 🔔 FCM token for push notifications
+    fcm_token = models.TextField(null=True, blank=True)
+
+    def __str__(self):
+        return self.username
